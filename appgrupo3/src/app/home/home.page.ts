eac5d26aa52845fc4fdbb46f661681bdcf50b84a
nilson-code <nilson.ledres.jr@gmail.com>
2019-12-07 15:57:42 -0300

    integração do banco com a tela

--- appgrupo3/src/app/home/home.page.ts
+++ appgrupo3/src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { DadosService } from '../services.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -7,11 +8,16 @@ import { DadosService } from '../services.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  
+  items: Observable<any[]>;
 
   constructor(public services:DadosService) {
+    
     services.getProdutos().subscribe((res:any) => {
       console.log(res);
+      this.items = res;
     })
+  
   }
 
 }
